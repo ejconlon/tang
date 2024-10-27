4da719d761a213e4efbc51054219a6c6c19074bc
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2024-10-26 18:29:51 -0700

    dot

--- tang/src/Tang/Dot.hs
+++ tang/src/Tang/Dot.hs
@@ -7,10 +7,12 @@ import Data.Foldable (for_, toList)
 import Data.List (intersperse)
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as Map
+import Data.Maybe (isNothing)
 import Data.Text (Text)
 import Data.Text.Lazy.Builder (Builder)
 import Data.Text.Lazy.Builder qualified as TLB
 import IntLike.Map qualified as ILM
+import IntLike.Set qualified as ILS
 import Tang.Ecta
   ( ChildIx (..)
   , Con (..)
@@ -29,13 +31,13 @@ import Tang.Render (RenderM, fromShowable, renderBuilder, renderBuilders)
 type Attrs = Map Text [Text]
 
 symbolNodeAttrs :: Attrs
-symbolNodeAttrs = Map.empty
+symbolNodeAttrs = Map.fromList [("shape", ["circle"])]
 
 choiceNodeAttrs :: Attrs
-choiceNodeAttrs = Map.empty
+choiceNodeAttrs = Map.fromList [("shape", ["square"])]
 
 cloneNodeAttrs :: Attrs
-cloneNodeAttrs = Map.empty
+cloneNodeAttrs = Map.fromList [("shape", ["octagon"])]
 
 rootNodeAttrs :: Attrs
 rootNodeAttrs = Map.fromList [("style", ["bold"])]
@@ -65,11 +67,13 @@ renderNode nid nlab mxlab attrs = do
     renderBuilders [" ", fromAttrs attrs]
   renderBuilder "]\n"
 
-renderEdge :: Builder -> Builder -> Attrs -> RenderM ()
-renderEdge nidSrc nidDest attrs = do
+renderEdge :: Builder -> Builder -> Maybe Builder -> Attrs -> RenderM ()
+renderEdge nidSrc nidDest mlab attrs = do
   renderBuilders ["  ", nidSrc, " -> ", nidDest]
-  unless (Map.null attrs) $
-    renderBuilders [" [", fromAttrs attrs, "]"]
+  unless (Map.null attrs && isNothing mlab) $ do
+    let labb = maybe mempty (\l -> "label=\"" <> l <> "\" ") mlab
+        atb = fromAttrs attrs
+    renderBuilders [" [", labb, atb, "]"]
   renderBuilder "\n"
 
 renderSeg :: Seg -> Builder
@@ -100,13 +104,22 @@ renderNodeGraph g _f (NodeGraph root m _) = do
     let attrs' = attrs <> (if i == root then rootNodeAttrs else mempty)
     renderNode nid nlab mxlab attrs'
   -- Emit edges
-  -- for_ (ILM.toList m) $ \(NodeId i, NodeInfo _sz _chi n) -> do
-  --   let _nid = fromShowable i
-  --   case n of
-  --     NodeSymbol (SymbolNode _ _fe) ->
-  --       todo
-  --     NodeChoice _js ->
-  --       todo
-  --     NodeClone _j ->
-  --       todo
+  for_ (ILM.toList m) $ \(i, NodeInfo sz cm lm n) -> do
+    let nid = fromShowable (unNodeId i)
+    case n of
+      NodeSymbol (SymbolNode _ _fe) -> do
+        for_ (fmap ChildIx [0 .. sz - 1]) $ \ix -> do
+          let mlab = fmap (TLB.fromText . unLabel) (ILM.lookup ix lm)
+          case Map.lookup (SegIndex ix) cm of
+            Nothing -> pure ()
+            Just cid -> do
+              let cidb = fromShowable (unNodeId cid)
+              renderEdge nid cidb mlab normalEdgeAttrs
+      NodeChoice js ->
+        for_ (ILS.toList js) $ \j -> do
+          let cidb = fromShowable (unNodeId j)
+          renderEdge nid cidb Nothing normalEdgeAttrs
+      NodeClone j -> do
+        let cidb = fromShowable (unNodeId j)
+        renderEdge nid cidb Nothing cloneEdgeAttrs
   renderBuilder "}\n"
