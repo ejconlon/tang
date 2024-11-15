df419a6a990eaf163a63355221def96492b991dd
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2024-11-15 09:06:46 -0800

    etc

--- tang/src/Tang/Test/Solver.hs
+++ tang/src/Tang/Test/Solver.hs
@@ -2,31 +2,38 @@
 
 module Tang.Test.Solver where
 
-import Control.Monad.IO.Class (liftIO)
-import PropUnit (TestTree, testUnit, (===))
+import PropUnit (TestTree, testGroup, testUnit, (===))
 import Tang.Exp (Exp (..))
 import Tang.Solver (SolveM, Sort (..), answer, assertions, newSolveSt, query, relation, rule, solve)
 import Z3.Base qualified as Z
 
-exampleRules :: SolveM ()
-exampleRules = do
+testSolver :: TestTree
+testSolver =
+  testGroup
+    "solver"
+    [ testRules1
+    ]
+
+exampleRules1 :: SolveM ()
+exampleRules1 = do
   relation "a" [] SortBool
   relation "b" [] SortBool
   relation "c" [] SortBool
   rule (ExpImplies "b" "a")
   rule (ExpImplies "c" "b")
 
-testSolver :: TestTree
-testSolver = testUnit "solver" $ do
+testRules1 :: TestTree
+testRules1 = testUnit "rules1" $ do
   ss <- newSolveSt
   resBefore <- solve ss $ do
-    exampleRules
+    exampleRules1
     query ["a"]
   resBefore === Z.Unsat
   resAfter <- solve ss $ do
     rule "c"
     query ["a"]
   resAfter === Z.Sat
-
--- solve ss answer >>= liftIO . print
--- solve ss assertions >>= liftIO . print
+  ans <- solve ss answer
+  ans === ExpBool True
+  ass <- solve ss assertions
+  ass === []
