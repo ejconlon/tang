b4eaee86ce6115ef7111701cbf2a4572fe73d092
Eric Conlon <37287+ejconlon@users.noreply.github.com>
2024-11-18 12:55:53 -0800

    etc

--- tang/src/Tang/Solver.hs
+++ tang/src/Tang/Solver.hs
@@ -1,5 +1,6 @@
 module Tang.Solver where
 
+import Control.Applicative (Alternative (..))
 import Control.Exception (Exception, throwIO)
 import Control.Monad ((>=>))
 import Control.Monad.Except (ExceptT, MonadError (..), runExceptT)
@@ -18,6 +19,7 @@ import Data.Set (Set)
 import Data.Set qualified as Set
 import Data.String (IsString (..))
 import Data.Tuple (swap)
+import ListT (ListT)
 import Tang.Exp (Tm (..), TmF (..), Ty (..))
 import Z3.Base qualified as ZB
 import Z3.Monad qualified as Z
@@ -111,23 +113,28 @@ data Err
 
 instance Exception Err
 
-newtype SolveM a = SolveM {unSolveM :: ReaderT SolveSt (ExceptT Err IO) a}
+newtype SolveT m a = SolveT {unSolveT :: ReaderT SolveSt (ExceptT Err m) a}
   deriving newtype (Functor, Applicative, Monad, MonadIO, MonadError Err)
 
-instance Z.MonadZ3 SolveM where
-  getSolver = SolveM (asks (rsSolver . ssRemote))
-  getContext = SolveM (asks (rsContext . ssRemote))
+type SolveM = SolveT IO
 
-instance Z.MonadFixedpoint SolveM where
-  getFixedpoint = SolveM (asks (rsFixedpoint . ssRemote))
+instance (MonadIO m) => Z.MonadZ3 (SolveT m) where
+  getSolver = SolveT (asks (rsSolver . ssRemote))
+  getContext = SolveT (asks (rsContext . ssRemote))
 
-instance MonadState LocalSt SolveM where
-  get = SolveM (ask >>= liftIO . readIORef . ssLocal)
-  put st = SolveM (ask >>= liftIO . flip writeIORef st . ssLocal)
-  state f = SolveM (ask >>= liftIO . flip atomicModifyIORef' (swap . f) . ssLocal)
+instance (MonadIO m) => Z.MonadFixedpoint (SolveT m) where
+  getFixedpoint = SolveT (asks (rsFixedpoint . ssRemote))
+
+instance (MonadIO m) => MonadState LocalSt (SolveT m) where
+  get = SolveT (ask >>= liftIO . readIORef . ssLocal)
+  put st = SolveT (ask >>= liftIO . flip writeIORef st . ssLocal)
+  state f = SolveT (ask >>= liftIO . flip atomicModifyIORef' (swap . f) . ssLocal)
+
+runSolveT :: SolveT m a -> SolveSt -> m (Either Err a)
+runSolveT m = runExceptT . runReaderT (unSolveT m)
 
 runSolveM :: SolveM a -> SolveSt -> IO (Either Err a)
-runSolveM m = runExceptT . runReaderT (unSolveM m)
+runSolveM = runSolveT
 
 withSolveM :: (MonadIO m) => SolveM a -> m a
 withSolveM m = newSolveSt >>= flip solve m
@@ -135,60 +142,60 @@ withSolveM m = newSolveSt >>= flip solve m
 solve :: (MonadIO m) => SolveSt -> SolveM a -> m a
 solve ss m = liftIO (runSolveM m ss >>= either throwIO pure)
 
-getM :: (LocalSt -> SolveM a) -> SolveM a
+getM :: (MonadIO m) => (LocalSt -> SolveT m a) -> SolveT m a
 getM f = do
-  r <- SolveM (asks ssLocal)
+  r <- SolveT (asks ssLocal)
   st0 <- liftIO (readIORef r)
   f st0
 
-stateM :: (LocalSt -> SolveM (a, LocalSt)) -> SolveM a
+stateM :: (MonadIO m) => (LocalSt -> SolveT m (a, LocalSt)) -> SolveT m a
 stateM f = do
-  r <- SolveM (asks ssLocal)
+  r <- SolveT (asks ssLocal)
   st0 <- liftIO (readIORef r)
   (a, st1) <- f st0
   liftIO (writeIORef r st1)
   pure a
 
-modifyM :: (LocalSt -> SolveM LocalSt) -> SolveM ()
+modifyM :: (MonadIO m) => (LocalSt -> SolveT m LocalSt) -> SolveT m ()
 modifyM f = do
-  r <- SolveM (asks ssLocal)
+  r <- SolveT (asks ssLocal)
   st0 <- liftIO (readIORef r)
   st1 <- f st0
   liftIO (writeIORef r st1)
 
-lookupDef :: String -> SolveM (Maybe ZDef)
+lookupDef :: (MonadIO m) => String -> SolveT m (Maybe ZDef)
 lookupDef name = gets (Map.lookup name . lsDefs)
 
-getDef :: String -> SolveM ZDef
+getDef :: (MonadIO m) => String -> SolveT m ZDef
 getDef name = do
   md <- lookupDef name
   case md of
     Nothing -> throwError (ErrMissingDef name)
     Just d -> pure d
 
-projectTm :: String -> ZDef -> SolveM ZTmDef
+projectTm :: (Monad m) => String -> ZDef -> SolveT m ZTmDef
 projectTm name = \case
   ZDefTm z -> pure z
   _ -> throwError (ErrNotTm name)
 
-projectTy :: String -> ZDef -> SolveM ZTyDef
+projectTy :: (Monad m) => String -> ZDef -> SolveT m ZTyDef
 projectTy name = \case
   ZDefTy z -> pure z
   _ -> throwError (ErrNotTm name)
 
-lookupTm :: String -> SolveM (Maybe ZTmDef)
+lookupTm :: (MonadIO m) => String -> SolveT m (Maybe ZTmDef)
 lookupTm name = lookupDef name >>= traverse (projectTm name)
 
-getTm :: String -> SolveM ZTmDef
+getTm :: (MonadIO m) => String -> SolveT m ZTmDef
 getTm name = getDef name >>= projectTm name
 
-lookupTy :: String -> SolveM (Maybe ZTyDef)
+lookupTy :: (MonadIO m) => String -> SolveT m (Maybe ZTyDef)
 lookupTy name = lookupDef name >>= traverse (projectTy name)
 
-getTy :: String -> SolveM ZTyDef
+getTy :: (MonadIO m) => String -> SolveT m ZTyDef
 getTy name = getDef name >>= projectTy name
 
-setDef :: String -> ZDef -> SolveM ()
+setDef :: (MonadIO m) => String -> ZDef -> SolveT m ()
 setDef name d = modifyM $ \ls ->
   case Map.lookup name ls.lsDefs of
     Nothing ->
@@ -196,7 +203,7 @@ setDef name d = modifyM $ \ls ->
       in  pure ls {lsDefs = defs}
     Just _ -> throwError (ErrDupeDef name)
 
-mkParams :: Params -> SolveM Z.Params
+mkParams :: (MonadIO m) => Params -> SolveT m Z.Params
 mkParams pm = do
   pz <- Z.mkParams
   for_ (Map.toList pm) $ \(k, v) -> do
@@ -208,7 +215,7 @@ mkParams pm = do
       PVString x -> Z.mkStringSymbol x >>= Z.paramsSetSymbol pz k'
   pure pz
 
-mkSym :: Sym -> SolveM Z.Symbol
+mkSym :: (MonadIO m) => Sym -> SolveT m Z.Symbol
 mkSym = \case
   SymNamed n -> Z.mkStringSymbol n
   SymAnon -> stateM $ \ls -> do
@@ -217,32 +224,32 @@ mkSym = \case
     x <- Z.mkIntSymbol i
     pure (x, st')
 
-getSort :: Ty -> SolveM Z.Sort
+getSort :: (MonadIO m) => Ty -> SolveT m Z.Sort
 getSort = \case
   TyVar n -> fmap (\(ZTyDef _ z) -> z) (getTy n)
   TyBool -> Z.mkBoolSort
   TyBv i -> Z.mkBvSort i
 
-getOrCreateSort :: String -> Maybe Ty -> SolveM Z.Sort
+getOrCreateSort :: (MonadIO m) => String -> Maybe Ty -> SolveT m Z.Sort
 getOrCreateSort name = \case
   Nothing -> Z.mkStringSymbol name >>= Z.mkUninterpretedSort
   Just ty -> getSort ty
 
-mkIntSort :: Ty -> SolveM Z.Sort
+mkIntSort :: (MonadIO m) => Ty -> SolveT m Z.Sort
 mkIntSort ty = do
   case ty of
     TyBv _ -> pure ()
     _ -> throwError (ErrNotIntTy ty)
   getSort ty
 
-mkFuncDecl :: String -> TmDef -> SolveM Z.FuncDecl
+mkFuncDecl :: (MonadIO m) => String -> TmDef -> SolveT m Z.FuncDecl
 mkFuncDecl name (TmDef _ args ret) = do
   name' <- Z.mkStringSymbol name
   args' <- traverse getSort args
   ret' <- getSort ret
   Z.mkFuncDecl name' args' ret'
 
-mkTmF :: TmF Z.AST -> SolveM Z.AST
+mkTmF :: (MonadIO m) => TmF Z.AST -> SolveT m Z.AST
 mkTmF = \case
   TmVarF x -> do
     ZTmDef (TmDef _ args _) fd' <- getTm x
@@ -270,10 +277,10 @@ mkTmF = \case
       then Z.mkApp fd' y
       else throwError (ErrArityMismatch x actualAr expectedAr)
 
-mkTm :: Tm -> SolveM Z.AST
+mkTm :: (MonadIO m) => Tm -> SolveT m Z.AST
 mkTm = cata (sequence >=> mkTmF)
 
-assertArity :: String -> [a] -> Int -> b -> SolveM b
+assertArity :: (Monad m) => String -> [a] -> Int -> b -> SolveT m b
 assertArity name as expected b =
   let actual = length as
   in  if actual == expected
@@ -285,7 +292,7 @@ data Arg = Arg !String !Ty
 
 type Env = IntMap Arg
 
-funcEnv :: String -> [String] -> SolveM Env
+funcEnv :: (MonadIO m) => String -> [String] -> SolveT m Env
 funcEnv funcName argNames = do
   ZTmDef (TmDef _ argTys _) _ <- getTm funcName
   let actual = length argTys
@@ -294,7 +301,7 @@ funcEnv funcName argNames = do
     then pure (IntMap.fromAscList (zip [0 ..] (fmap (uncurry Arg) (zip argNames argTys))))
     else throwError (ErrArityMismatch funcName actual expected)
 
-reflectTy :: Z.Sort -> SolveM Ty
+reflectTy :: (MonadIO m) => Z.Sort -> SolveT m Ty
 reflectTy s = do
   k <- Z.getSortKind s
   case k of
@@ -313,12 +320,12 @@ reflectTy s = do
     -- Z3_UNKNOWN_SORT
     _ -> error "TODO expand reflectTy"
 
-reflectIntTy :: Ty -> Z.AST -> SolveM Tm
+reflectIntTy :: (MonadIO m) => Ty -> Z.AST -> SolveT m Tm
 reflectIntTy ty t = case ty of
   TyBv _ -> fmap (TmInt ty . fromInteger) (Z.getInt t)
   _ -> throwError (ErrNotIntTy ty)
 
-reflectTm :: Env -> Z.AST -> SolveM Tm
+reflectTm :: (MonadIO m) => Env -> Z.AST -> SolveT m Tm
 reflectTm env = go
  where
   go t = do
@@ -353,34 +360,34 @@ reflectTm env = go
         reflectIntTy ty t
       _ -> throwError (ErrReflect ("Unsupported type: " ++ show k))
 
-defVar :: String -> Ty -> SolveM ()
+defVar :: (MonadIO m) => String -> Ty -> SolveT m ()
 defVar name = defFun' RoleVar name []
 
-defConst :: String -> Ty -> SolveM ()
+defConst :: (MonadIO m) => String -> Ty -> SolveT m ()
 defConst name = defFun name []
 
-defFun :: String -> [Ty] -> Ty -> SolveM ()
+defFun :: (MonadIO m) => String -> [Ty] -> Ty -> SolveT m ()
 defFun = defFun' RoleUninterp
 
-defFun' :: Role -> String -> [Ty] -> Ty -> SolveM ()
+defFun' :: (MonadIO m) => Role -> String -> [Ty] -> Ty -> SolveT m ()
 defFun' role name args ret = do
   let tmd = TmDef role args ret
   fd' <- mkFuncDecl name tmd
   setDef name (ZDefTm (ZTmDef tmd fd'))
 
-defTy :: String -> Maybe Ty -> SolveM ()
+defTy :: (MonadIO m) => String -> Maybe Ty -> SolveT m ()
 defTy name mty = do
   sort' <- getOrCreateSort name mty
   setDef name (ZDefTy (ZTyDef (TyDef mty) sort'))
 
-defRel :: String -> [Ty] -> SolveM ()
+defRel :: (MonadIO m) => String -> [Ty] -> SolveT m ()
 defRel name args = do
   let tmd = TmDef RoleRel args TyBool
   fd' <- mkFuncDecl name tmd
   Z.fixedpointRegisterRelation fd'
   setDef name (ZDefTm (ZTmDef tmd fd'))
 
-gatherVars :: Tm -> SolveM (Set String)
+gatherVars :: (MonadIO m) => Tm -> SolveT m (Set String)
 gatherVars tm0 = execStateT (cata go tm0) Set.empty
  where
   go tm = case tm of
@@ -391,7 +398,7 @@ gatherVars tm0 = execStateT (cata go tm0) Set.empty
         _ -> pure ()
     _ -> sequence_ tm
 
-defRule :: Tm -> [Tm] -> SolveM ()
+defRule :: (MonadIO m) => Tm -> [Tm] -> SolveT m ()
 defRule hd tl = do
   let e = case tl of
         [] -> hd
@@ -408,12 +415,12 @@ defRule hd tl = do
   s' <- mkSym SymAnon
   Z.fixedpointAddRule q' s'
 
-query :: String -> SolveM Z.Result
+query :: (MonadIO m) => String -> SolveT m Z.Result
 query funcName = do
   decl' <- fmap (\(ZTmDef _ z) -> z) (getTm funcName)
   Z.fixedpointQueryRelations [decl']
 
-answer :: String -> [String] -> SolveM (Maybe Tm)
+answer :: (MonadIO m) => String -> [String] -> SolveT m (Maybe Tm)
 answer funcName argNames = do
   env <- funcEnv funcName argNames
   res <- query funcName
@@ -421,5 +428,35 @@ answer funcName argNames = do
     Z.Sat -> fmap Just (Z.fixedpointGetAnswer >>= reflectTm env)
     _ -> pure Nothing
 
-params :: Params -> SolveM ()
+params :: (MonadIO m) => Params -> SolveT m ()
 params = mkParams >=> Z.fixedpointSetParams
+
+-- private
+push :: (MonadIO m) => SolveT m LocalSt
+push = do
+  ls <- get
+  Z.push
+  pure ls
+
+-- private
+pop :: (MonadIO m) => LocalSt -> SolveT m ()
+pop ls = do
+  Z.pop 1
+  put ls
+
+newtype SolveListM a = SolveListM {unSolveListM :: ListT SolveM a}
+  deriving newtype (Functor, Applicative, Monad)
+
+instance Alternative SolveListM where
+  empty = SolveListM empty
+  s1 <|> s2 = SolveListM (unSolveListM (restoring s1) <|> unSolveListM (restoring s2))
+
+-- private
+ensuring :: SolveM () -> ListT SolveM a -> ListT SolveM a
+ensuring cleanup act = catchError act (\e -> lift cleanup >> throwError e) >>= \a -> a <$ lift cleanup
+
+-- private
+restoring :: SolveListM a -> SolveListM a
+restoring (SolveListM act) = SolveListM $ do
+  ls <- lift push
+  ensuring (pop ls) act
